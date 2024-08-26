a0761a4ebcfd4671e3bf2f526e93022d3d1864fd
blueofsky <myjobmails@163.com>
2024-08-26 14:10:01 +0800

    feat: add finetune code

--- finetune/bert-base-cased-finetune-yelp-inference.py
+++ finetune/bert-base-cased-finetune-yelp-inference.py
@@ -17,9 +17,13 @@ def inference(text):
     logits = outputs.logits
     probabilities = torch.nn.functional.softmax(logits, dim=1)
     predicted_label_idx = torch.argmax(probabilities, dim=1).item()
+    
     class_names = ["LABEL_0", "LABEL_1", "LABEL_2", "LABEL_3", "LABEL_4"]
+    scores = probabilities[0].tolist()
+    my_dict = {k: v for k, v in zip(class_names, scores)}
+    
     predicted_label_name = class_names[predicted_label_idx]
-    return predicted_label_name
+    return predicted_label_name,my_dict[predicted_label_name]
 
 def pipd(text):
     pipe = pipeline(task="sentiment-analysis", tokenizer=origin_model, model=finetune_model)
@@ -27,7 +31,7 @@ def pipd(text):
 
 if __name__ == '__main__':    
     text = "One of the best pizzas I've had in Charlotte. They also sell pizza by the slice which is a plus"
-    predicted_label_idx=inference(text)
-    print(f"Predicted label name: {predicted_label_idx}")
+    predicted_label_idx,score=inference(text)
+    print(f"Predicted label: 'label': {predicted_label_idx}, 'score': {score:.4f}")
     predicted_label_idx=pipd(text)
     print(f"Predicted label: {predicted_label_idx}")
