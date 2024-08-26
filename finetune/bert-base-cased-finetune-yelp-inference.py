ec39fddce8423c3092eb64cd181f14f4960d65f1
blueofsky <myjobmails@163.com>
2024-08-26 13:30:27 +0800

    feat: add finetune code

--- finetune/bert-base-cased-finetune-yelp-inference.py
+++ finetune/bert-base-cased-finetune-yelp-inference.py
@@ -17,7 +17,9 @@ def inference(text):
     logits = outputs.logits
     probabilities = torch.nn.functional.softmax(logits, dim=1)
     predicted_label_idx = torch.argmax(probabilities, dim=1).item()
-    return predicted_label_idx
+    class_names = ["LABEL_0", "LABEL_1", "LABEL_2", "LABEL_3", "LABEL_4"]
+    predicted_label_name = class_names[predicted_label_idx]
+    return predicted_label_name
 
 def pipd(text):
     pipe = pipeline(task="sentiment-analysis", tokenizer=origin_model, model=finetune_model)
