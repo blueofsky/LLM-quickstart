e1919854d063d3bc74772cc67f0b98498de79d3f
blueofsky <myjobmails@163.com>
2024-08-31 12:24:59 +0800

    feat: add base

---
+++ finetune/finetune_base.py
@@ -0,0 +1,13 @@
+import torch
+from transformers import AutoModelForCausalLM
+
+model_name_or_path = 'THUDM/chatglm3-6b'   # 模型ID或本地路径
+model = AutoModelForCausalLM.from_pretrained(model_name_or_path, device_map="cuda").to(0)
+
+for param in model.parameters():
+    print(f'Parameter dtype: {param.dtype}')
+    break 
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+initial_memory_usage = torch.cuda.memory_allocated(device)
+print(f"Initial memory usage: {initial_memory_usage} bytes")
