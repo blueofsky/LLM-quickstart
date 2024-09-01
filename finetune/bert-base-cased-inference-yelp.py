969062f1b080149a755f0d9584343d39d2704570
blueofsky <myjobmails@163.com>
2024-09-01 08:06:17 +0800

    feat: add jupyter notebook

--- finetune/bert-base-cased-inference-yelp.py
+++ finetune/bert-base-cased-inference-yelp.py
@@ -9,24 +9,38 @@ origin_model="bert-base-cased"
 finetune_model="models/bert-base-cased-finetune-yelp"
 
 def inference(text):
+    # 模型加载
     tokenizer = AutoTokenizer.from_pretrained(origin_model)
     model = AutoModelForSequenceClassification.from_pretrained(finetune_model)
+    
+    #使用分词器将输入文本转换为模型所需的格式，并指定返回的张量类型为PyTorch张量（"pt"代表PyTorch）
+    #这里的inputs变量将包含分词器处理后的结果，它是一个字典，包含分词器生成的编码张量、注意力掩码（attention mask）等信息
     inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
+    
+    # 模型推理
     with torch.no_grad():
         outputs = model(**inputs)
+    ##获取了模型的输出logits。Logits是模型在每个可能的类别上的得分或概率，还没有经过概率归一化处理
     logits = outputs.logits
-    probabilities = torch.nn.functional.softmax(logits, dim=1)
+    
+    # 归一化
+    ## 使用PyTorch中的torch.softmax函数对logits进行softmax操作。softmax函数将每个logit值转换为在[0, 1]范围内的概率值，使得所有类别的概率之和为1
+    ## dim=-1表示在最后一个维度上进行softmax操作。在情感分类任务中，通常最后一个维度是类别维度
+    probabilities = torch.softmax(logits, dim=-1)
+    #使用argmax找到logits中概率最高的类别索引，并通过.item()将其转换为一个普通的Python整数。
     predicted_label_idx = torch.argmax(probabilities, dim=1).item()
     
-    class_names = ["LABEL_0", "LABEL_1", "LABEL_2", "LABEL_3", "LABEL_4"]
-    scores = probabilities[0].tolist()
-    my_dict = {k: v for k, v in zip(class_names, scores)}
+    #使用预测得到的类别索引predicted_label_idx从id2label映射中获取对应的标签。
+    print('id2label: ',model.config.id2label)
+    predicted_label_name = model.config.id2label.get(predicted_label_idx)
     
-    predicted_label_name = class_names[predicted_label_idx]
-    return predicted_label_name,my_dict[predicted_label_name]
+    # score
+    score = probabilities[0].tolist()[predicted_label_idx]
+    return predicted_label_name,score
 
 def pipd(text):
-    pipe = pipeline(task="sentiment-analysis", tokenizer=origin_model, model=finetune_model)
+    pipe = pipeline(task="sentiment-analysis", tokenizer=origin_model, model=finetune_model, device=0)
+    print('pipeline use: ',pipe.model.device)
     return pipe(text)
 
 if __name__ == '__main__':    
